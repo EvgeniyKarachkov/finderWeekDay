8aa6f73e49a9aad2dfd3a68a003c22c4e2c3b93c
EvgeniyKarachkov <cazinak341@gmail.com>
2021-04-24 06:45:03 +0300

    Added project

---
+++ weekFinder/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  weekFinder
+//
+//  Created by Zodino BLR on 4/23/21.
+//  Copyright © 2021 Zodino BLR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findweekDay() {
+        
+        let calendar = Calendar.current
+        
+        var dateComponents = DateComponents()
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekday = dateFormatter.string(from: date)
+        
+        resultLabel.text = weekday
+    
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+}
+
